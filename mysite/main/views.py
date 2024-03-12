644237de2b1a331a14bbc1092a663c57f81d7ad2
Musarraf <muhammedmusarraf@gmail.com>
2024-03-12 16:41:36 +0530

    Added Modes for Database

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Welcome Musarraf</h1>")
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return HttpResponse("<h1>%s</h1>" %ls.name)
 
-def v1(response):
-    return HttpResponse("<h1>View 1!</h1>")
